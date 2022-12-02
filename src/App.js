8e51a160f2c89982bd49c0bf97aa3d2175b39933
aldoprogrammer <aldols@bugcrowdninja.com>
2022-12-02 11:06:55 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+import "antd/dist/antd.css";
+
+function App() {
+  
+    
+  return (
+    <div className="App">
+      <h2>Country Search</h2>
+       
+      
+      
+      </div>
+  );
+  
+}
+
+export default App;
+// npm install --save antd
